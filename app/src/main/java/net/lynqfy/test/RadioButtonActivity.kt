8eaba29e5ba7c8eab13bb05a9a0837f5a72f34be
t4-morteza <m.bayat@trader4.net>
2023-04-28 06:10:17 +0330

    added LyRadioGroup Widget Ui.

--- app/src/main/java/net/lynqfy/test/RadioButtonActivity.kt
+++ app/src/main/java/net/lynqfy/test/RadioButtonActivity.kt
@@ -2,10 +2,27 @@ package net.lynqfy.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioGroup
+import net.lynqfy.offical.radio.LyRadio
+import net.lynqfy.offical.radio.LyRadioGroup
+import java.util.Timer
 
 class RadioButtonActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_radio_button)
+
+        val radioGroup = findViewById<LyRadioGroup>(R.id.radioGroup)
+        val radio1 = findViewById<LyRadio>(R.id.radio1)
+        val radio2 = findViewById<LyRadio>(R.id.radio2)
+
+        Log.e("FF" , " ${radio1.id} ,radio2 ${radio2.id}")
+        Log.e("FF" , " ${radio1.getHelper()} ,radio2 ${radio2.getHelper()}")
+
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            Log.e("FF" , "$group , $checkedId")
+        }
     }
 }
--- lynqfy/src/main/java/net/lynqfy/offical/radio/LyRadio.kt
+++ lynqfy/src/main/java/net/lynqfy/offical/radio/LyRadio.kt
@@ -3,13 +3,14 @@ package net.lynqfy.offical.radio
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.CompoundButton
-import androidx.appcompat.content.res.AppCompatResources
-import com.google.android.material.card.MaterialCardView
+import android.widget.LinearLayout
+import com.google.android.material.radiobutton.MaterialRadioButton
 import net.lynqfy.offical.R
 import net.lynqfy.offical.databinding.LyRadioUiBinding
 
-class LyRadio : MaterialCardView {
+class LyRadio : LinearLayout {
 
     constructor(ctx: Context) : super(ctx, null) {
         initTheme(null)
@@ -28,11 +29,14 @@ class LyRadio : MaterialCardView {
     }
 
     fun setOnCheckedChangeListener(l: CompoundButton.OnCheckedChangeListener) {
-        ui.radioBtn.setOnCheckedChangeListener(l)
+        radioBtn.setOnCheckedChangeListener(l)
     }
 
     private fun initTheme(attrs: AttributeSet?, defStyleAttr: Int = 0) {
         // Ensure we are using the correctly themed context rather than the context that was passed in.
+
+        radioBtn =  MaterialRadioButton(context,attrs)
+
         attrs?.also { att ->
             val attributes = context.obtainStyledAttributes(
                 att,
@@ -41,27 +45,31 @@ class LyRadio : MaterialCardView {
                 com.google.android.material.R.style.Widget_MaterialComponents_CardView
             )
 
-            ui.radioBtn.text = attributes.getString(R.styleable.LyRadio_radioName)
+            radioBtn.text = attributes.getString(R.styleable.LyRadio_radioName)
             ui.helperTv.text = attributes.getString(R.styleable.LyRadio_radioHelper)
 
             val isRadioDisable = attributes.getBoolean(R.styleable.LyRadio_radioDisable, false)
             if (isRadioDisable) {
-//                ui.radioBtn.buttonDrawable =
-//                    AppCompatResources.getDrawable(context, R.drawable.ic_radio_disable)
-                ui.radioBtn.isEnabled = false
-                ui.radioBtn.setTextColor(context.getColor(R.color.ly_card_content))
+                radioBtn.isEnabled = false
+                radioBtn.setTextColor(context.getColor(R.color.ly_card_content))
             } else {
-                ui.radioBtn.setTextColor(context.getColor(R.color.ly_card_title))
-//                ui.radioBtn.buttonDrawable =
-//                    AppCompatResources.getDrawable(context, R.drawable.ic_radio_enable)
+                radioBtn.setTextColor(context.getColor(R.color.ly_card_title))
             }
             attributes.recycle()
         }
 
-        radius = 18f
-        useCompatPadding = true
+        ui.radioParent.addView(radioBtn)
         addView(ui.root)
     }
+
+    fun isChecked() = radioBtn.isChecked
+    fun setChecked(v: Boolean) {
+        radioBtn.isChecked = v
+    }
+
+    fun getHelper(): CharSequence? = ui.helperTv.text
+
+    private lateinit var radioBtn  : MaterialRadioButton
 
     private val ui by lazy {
         LyRadioUiBinding.inflate(LayoutInflater.from(context), this, false)
